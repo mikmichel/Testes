27bbb5b185033f7ec4a3a5b5cf02a2577c1619b0
Michael Angelo_Note_Pessoal <mikmichel@gmail.com>
2023-10-06 20:55:31 -0300

    Escreva um programa que leia um valor em metros e o exiba convertido em milímetros

---
+++ Programação para Redes/Teste Python/ListaTeste/L1_E2.py
@@ -0,0 +1,11 @@
+print("\tConversão de Metros para Milímetros\n")
+
+#Inserindo valor
+metros = float(input("Metros: "))
+
+#Calculando conversão
+milimetros = (metros * 1000)
+
+#Imprimindo resultado na tela
+print("")
+print("Conversão em milímetros: %6.2f" %milimetros)
