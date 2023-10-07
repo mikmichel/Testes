84dfc94579acd23d45c7c4efa059aed5b4c31f23
Michael Angelo_PC_TRABALHO <mikmichel@gmail.com>
2023-10-07 16:08:31 -0300

    Sabendo que str( ) converte valores numéricos para string, calcule quantos dígitos há em 2 elevado a um milhão.

---
+++ Programação para Redes/Teste Python/ListaTeste/L1_E11.py
@@ -0,0 +1,4 @@
+#"Sabendo que str( ) converte valores numéricos para string, calcule quantos dígitos há em 2 elevado a um milhão."
+
+
+print ('2 elevado a 1 milhão tem %d digitos' %len(str(2**1000000)) )
