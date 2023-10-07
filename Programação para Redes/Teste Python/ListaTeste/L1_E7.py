cab540dbf7076226fb8ff70cc24f9e830096b613
Michael Angelo_PC_TRABALHO <mikmichel@gmail.com>
2023-10-07 16:00:13 -0300

    Converta uma temperatura digitada em Celsius para Fahrenheit. F = 9*C/5 + 32

---
+++ Programação para Redes/Teste Python/ListaTeste/L1_E7.py
@@ -0,0 +1,7 @@
+# Converta uma temperatura digitada em Celsius para Fahrenheit. F = 9*C/5 + 32 
+##############################################################################
+
+c = input("Digite a temperatua em Celsius: ")
+f = 9*c/5 + 32
+
+print ('%d Fahrenheit' %f)
